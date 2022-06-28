3bc96ee50cd07660178709faa77bab7c82f8d529
oguna <nao.lk.118@gmail.com>
2022-06-28 22:55:05 +0900

    add test

---
+++ test/FreeYubin.test.ts
@@ -0,0 +1,13 @@
+import { CompactDictionary } from "../src/CompactDictionary";
+import { readFileSync } from "fs";
+
+describe("freeyubin", () => {
+    const buff = readFileSync('freeyubin-dict-20220531');
+    const dict = new CompactDictionary(buff.buffer);
+
+    it("5650871", () => {
+        const iter = dict.search('5650871')
+        const address = iter.next().value
+        expect(address).toEqual('大阪府 吹田市 山田丘');
+    });
+});
